22ccbcaf3f68a8e32565aedb7e01eab2aadb59c7
Leonardo Testi <ltesti120a@gmail.com>
2022-09-27 16:18:05 +0200

    [utils] added settl-restricted and nextgen-restricted
    Tested

--- SynthOptSpec/utils.py
+++ SynthOptSpec/utils.py
@@ -85,6 +85,11 @@ def get_spec_file(teff,LogG,modspecdir=None,oldgrid=False,model='Settl', in_dict
                 if not modspecdir:
                     modspecdir = 'Models/bt-settl/'
                 specfile = modspecdir + 'lte' + teffstr + '.0-' + LogG + '-' + Z + 'a+0.0.BT-Settl.spec.7.edit'
+            elif model == 'Settl-restricted':
+                if not modspecdir:
+                    modspecdir = 'Models/bt-settl-restricted/'
+                modflux_log = False
+                specfile = modspecdir + 'lte' + teffstr + '-' + LogG + '-' + Z + 'a+0.0.BT-Settl.spec.7.dat.txt'
             elif model == 'Settl-2019':
                 if not modspecdir:
                     modspecdir = 'Models/bt-settl-2019/'
@@ -93,6 +98,11 @@ def get_spec_file(teff,LogG,modspecdir=None,oldgrid=False,model='Settl', in_dict
                 if not modspecdir:
                     modspecdir = 'Models/bt-nextgen/'
                 specfile = modspecdir + 'lte' + teffstr + '-' + LogG + '-' + Z + 'a+0.0.BT-NextGen.7'
+            elif model == 'NextGen-restricted':
+                if not modspecdir:
+                    modspecdir = 'Models/bt-nextgen-restricted/'
+                modflux_log = False
+                specfile = modspecdir + 'lte' + teffstr + '-' + LogG + '-' + Z + 'a+0.0.BT-NextGen.7.dat.txt'
             elif model == 'Cond':
                 if not modspecdir:
                     modspecdir = 'Models/bt-cond/'
